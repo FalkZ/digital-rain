6a3430a9a93459365d72bad3eb90f9229ad872eb
Falk Zwimpfer <24669719+FalkZ@users.noreply.github.com>
2018-06-03 16:19:28 +0200

    added white glowing

--- matrix.js
+++ matrix.js
@@ -1,10 +1,46 @@
 const config = {
 	width: window.innerWidth,
 	height: window.innerHeight,
-	redraw: 100,
+	redraw: 80,
 	spacingX: 20,
 	spacingY: 20,
-	chars: ['る', '見', 'い', '白', 'か', 'っ', 'た', '可']
+	chars: [
+		0,
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		'+',
+		'M',
+		'A',
+		'T',
+		'R',
+		'I',
+		'X',
+		'る',
+		'見',
+		'い',
+		'白',
+		'か',
+		'っ',
+		'た',
+		'可',
+		'べ',
+		'て',
+		'み',
+		'コ',
+		'ン',
+		'ピ',
+		'ュ',
+		'ー',
+		'タ'
+	],
+	mono: loadFont('share-tech-mono.otf')
 }
 
 const getNegativeRandomInt = (max) => getRandomInt(max) * -1
@@ -19,6 +55,8 @@ function setup() {
 	rect(0, 0, config.width, config.height)
 
 	textSize(20)
+	textAlign(CENTER, CENTER)
+
 	setTimeout(drawLetters, config.redraw)
 }
 
@@ -30,6 +68,7 @@ function draw() {
 const incrementY = (y) => (y < config.height ? y + config.spacingX : 0)
 
 let y = []
+let last = []
 
 const drawLetters = () => {
 	setTimeout(drawLetters, config.redraw)
@@ -43,8 +82,24 @@ const drawLetter = (index) => {
 		y[index] = getNegativeRandomInt(40 * config.spacingY)
 	}
 
+	let char = getRandomChar()
+
 	fill(45, 255, 94)
-	text(getRandomChar(), index * config.spacingX, y[index])
+
+	text(last[index], index * config.spacingX, y[index])
 
 	y[index] = incrementY(y[index])
+
+	//fill(100)
+
+	//textSize(22)
+	//text(char, index * config.spacingX, y[index])
+
+	fill(255)
+
+	textSize(21)
+
+	text(char, index * config.spacingX, y[index])
+
+	last[index] = char
 }
